475c2cac330ea34fcfba859fbd4e80cb2e04a8a9
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-01-12 22:47:19 +0530

    trying first time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.meth();
+        ob.meth();
+    }
+}
+
+class A{
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
